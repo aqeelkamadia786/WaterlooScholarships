bc0ee563795f5cacfa81888c799d444f1def7463
Aqeel Kamadia <aqeelkamadia@v1020-wn-158-159.campus-dynamic.uwaterloo.ca>
2018-03-14 15:44:12 -0400

    Completely changed layout of details page

---
+++ WaterlooScholarships/DetailTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DetailTableViewCell.swift
+//  WaterlooScholarships
+//
+//  Created by Aqeel Kamadia on 2018-03-14.
+//  Copyright © 2018 Aqeel Kamadia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- WaterlooScholarships/DetailsViewController.swift
+++ WaterlooScholarships/DetailsViewController.swift
@@ -9,34 +9,45 @@
 import Foundation
 import UIKit
 
-class DetailsViewController: UIViewController {
+enum DetailSection {
+    case title
+    case value
+    case description
+    case programs
+}
+
+class DetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var valueLabel: UILabel!
-    @IBOutlet weak var favouriteButton: UIButton!
-    @IBOutlet weak var programLabel: UILabel!
+    @IBOutlet weak var favouriteButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
+    var detailSection = DetailSection.title
+    let titles = ["Title", "Value", "Description", "Programs"]
     var scholarship: Scholarship?
+    var details: [[String]] = []
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         setupDetails()
     }
     
     func setupDetails() {
         if let scholarship = scholarship {
             title = scholarship.title
-            titleLabel.text = scholarship.title
-            valueLabel.text = scholarship.value
-            descriptionLabel.text = scholarship.details
-            programLabel.text = "Program(s):"
-            for program in scholarship.programs {
-                programLabel.text?.append("\n" + program)
-            }
-            if scholarship.favourited {
-                favouriteButton.isEnabled = false
-                favouriteButton.alpha = 0.3
+            details = [[scholarship.title], [scholarship.value], [scholarship.details], scholarship.programs]
+            if let favouritesData = defaults.object(forKey: "Favourites") as? Data {
+                let array = NSKeyedUnarchiver.unarchiveObject(with: favouritesData) as! [Scholarship]
+                for item in array {
+                    if item.title == scholarship.title {
+                        scholarship.favourited = true
+                        favouriteButton.isEnabled = false
+                        return
+                    }
+                }
+                scholarship.favourited = false
+                favouriteButton.isEnabled = true
             }
         }
     }
@@ -55,11 +66,37 @@ class DetailsViewController: UIViewController {
             defaults.synchronize()
             if scholarship.favourited {
                 favouriteButton.isEnabled = false
-                favouriteButton.alpha = 0.3
             }   else {
                 favouriteButton.isEnabled = true
-                favouriteButton.alpha = 1.0
             }
         }
+        presentModal()
     }
+    
+    func presentModal() {
+        let popup = UIAlertController(title: "Favourited", message: "This scholarship has been added to your favourites", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        popup.addAction(action)
+        present(popup, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return details[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return titles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailTableViewCell", for: indexPath) as! DetailTableViewCell
+        cell.label.text = details[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    
 }
